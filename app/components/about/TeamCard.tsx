9a1b57809af7118f5b5ba772107c797f9d6497e5
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-05-28 10:58:44 +0000

    Fix typescript bug

--- app/components/about/TeamCard.tsx
+++ app/components/about/TeamCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image';
 
-export default function TeamCard({member}) {
+export default function TeamCard({member} : {member: Member}) {
   return (
     <div>
         <figure className='relative'>
