f4cbdc4b1649ad12c04c290956194d5f7f7bbb49
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-05-28 07:22:41 +0000

    Create about hero section

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//components
+import AboutHero from '../components/about/AboutHero'
+
+export default function About() {
+  return (
+    <main className='about-page '>
+        <AboutHero />
+    </main>
+  )
+}
---
+++ app/components/about/AboutHero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function AboutHero() {
+  return (
+    <section className='contact-hero py-[30px] xsm:py-[40px] md:py-[50px] sm-lg:py-[90px]'>
+        <div className='text-center sm-lg:max-w-[70%] mx-auto'>
+            <h1 className='leading-tight text-[26px] xsm:text-[40px] md:text-[60px] mb-[10px]'>
+                Building <span className='text-primaryBlue'>connectivity</span>, delivering value
+            </h1>
+        </div>
+    </section>
+  )
+}
