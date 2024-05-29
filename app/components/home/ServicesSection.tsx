fd852ff6074398e5533030caf30317f078cd6e93
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-05-29 16:15:08 +0000

    add inline padding to services section

--- app/components/home/ServicesSection.tsx
+++ app/components/home/ServicesSection.tsx
@@ -17,7 +17,7 @@ const renderServices = SERVICES.map((service, index) => (
 export default function ServicesSection() {
   return (
     <div className='-mx-[15px] md:-mx-[20px] sm-lg:-mx-[30px] lg:-mx-[50px] x-lg:-mx-[70px]'>
-            <figure className='relative ml-auto -mt-[130px] mb-20 w-[85%]'>
+            <figure className='hidden sm-lg:block relative ml-auto -mt-[130px] mb-20 w-[85%]'>
                 <Image 
                     src={'/path2.svg'}
                     alt=''
@@ -26,7 +26,7 @@ export default function ServicesSection() {
                     className='w-full h-auto right-0'
                 />
             </figure>
-        <section className='max-w-[1280px] mx-auto pb-[50px] md:pb-[70px] lg:pb-[100px]'>
+        <section className='services-section-content max-w-[1280px] mx-auto pb-[50px] md:pb-[70px] lg:pb-[100px]'>
             <div className='mb-[30px]'>
                 <h2 className='mb-[10px] text-[36px] xsm:text-[40x] md:text-[48px] xl:text-[52px]'>
                     Shipping and Logistic services
