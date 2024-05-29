8c95a208a14858dbfbe44c631740858a33423cba
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-05-29 16:11:33 +0000

    Add road image under services section

--- app/components/home/ServicesSection.tsx
+++ app/components/home/ServicesSection.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 //data
 import {SERVICES} from '@/app/data/services'
@@ -15,15 +16,27 @@ const renderServices = SERVICES.map((service, index) => (
 
 export default function ServicesSection() {
   return (
-    <section className='max-w-[1280px] mx-auto pb-[50px] md:pb-[70px] lg:pb-[100px]'>
-        <div className='mb-[30px]'>
-            <h2 className='mb-[10px] text-[36px] xsm:text-[40x] md:text-[48px] xl:text-[52px]'>
-                Shipping and Logistic services
-            </h2>
-        </div>
-        <ul className='grid gap-[30px] sm-lg:gap-[60px]'>
-            {renderServices}
-        </ul>
-    </section>
+    <div className='-mx-[15px] md:-mx-[20px] sm-lg:-mx-[30px] lg:-mx-[50px] x-lg:-mx-[70px]'>
+            <figure className='relative ml-auto -mt-[130px] mb-20 w-[85%]'>
+                <Image 
+                    src={'/path2.svg'}
+                    alt=''
+                    width={840}
+                    height={416}
+                    className='w-full h-auto right-0'
+                />
+            </figure>
+        <section className='max-w-[1280px] mx-auto pb-[50px] md:pb-[70px] lg:pb-[100px]'>
+            <div className='mb-[30px]'>
+                <h2 className='mb-[10px] text-[36px] xsm:text-[40x] md:text-[48px] xl:text-[52px]'>
+                    Shipping and Logistic services
+                </h2>
+            </div>
+            <ul className='grid gap-[30px] sm-lg:gap-[60px]'>
+                {renderServices}
+            </ul>
+
+        </section>
+    </div>
   )
 }
