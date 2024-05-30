b8149c926447f59aab60c8fd514fc54235cd0c36
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-05-30 23:17:17 +0000

    Fix blog page bug

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,43 +1,40 @@
-'use client'
+'use client';
 
-import React, { Suspense, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { BLOGS } from '@/app/data/blog';
 import BlogCard from '../components/home/BlogCard';
 
 function BlogContent() {
     const searchParams = useSearchParams();
-    const paramValue = searchParams.get('page');
     const router = useRouter();
+    const paramValue = searchParams.get('page');
 
-    console.log(paramValue);
+    const [pageNumber, setPageNumber] = useState(1);
 
-    let [pageNumber, setPageNumber] = useState((paramValue && paramValue !== '') ? parseInt(paramValue) : 1);
-
-    console.log("(paramValue && paramValue !== '') ? parseInt(paramValue) : 1: ", (paramValue && paramValue !== '') ? parseInt(paramValue) : 1);
-
-    console.log("pageNumber: ", pageNumber);
+    useEffect(() => {
+        if (paramValue && paramValue !== '') {
+            setPageNumber(parseInt(paramValue));
+        }
+    }, [paramValue]);
 
     const goToNextPage = () => {
         router.push(`/blog?page=${pageNumber + 1}`);
-    }
+    };
 
     const goToPrevPage = () => {
         router.push(`/blog?page=${pageNumber - 1}`);
-    }
+    };
 
     const renderBlogs = () => {
-        return (
-            [...BLOGS].slice((6 * pageNumber) - 6, (6 * pageNumber) > BLOGS.length ? BLOGS.length : (6 * pageNumber)).map(blog => (
-                <li
-                    key={blog.title}
-                    className='group cursor-pointer'
-                >
-                    <BlogCard blog={blog} />
-                </li>
-            ))
-        );
-    }
+        const startIndex = (pageNumber - 1) * 6;
+        const endIndex = Math.min(pageNumber * 6, BLOGS.length);
+        return BLOGS.slice(startIndex, endIndex).map((blog) => (
+            <li key={blog.title} className='group cursor-pointer'>
+                <BlogCard blog={blog} />
+            </li>
+        ));
+    };
 
     return (
         <main>
@@ -45,32 +42,32 @@ function BlogContent() {
                 <h1 className='text-center my-[30px] xsm:my-20 md:my-[50px] sm-lg:my-[90px]'>
                     Blog
                 </h1>
-
                 <section className=''>
-                    <ul className='grid gap-[20px] xl:gap-[50px] md:grid-cols-2 lg:grid-cols-3'>
+                    <ul className='grid gap-[20px] md:grid-cols-2 lg:grid-cols-3'>
                         {renderBlogs()}
                     </ul>
-
                     <div className='mt-[60px] md:mt-[70px] sm-lg:mt-[80px] flex gap-5 justify-between items-center'>
-                        {
-                            pageNumber > 1 ?
-                                <button className='bg-primaryBlue w-full max-w-[350px] py-3 px-[34px] text-[#f6f6f6]' onClick={goToPrevPage}>
-                                    Previous
-                                </button>
-                                :
-                                <div className='w-full max-w-[350px]'></div>
-                        }
-
+                        {pageNumber > 1 ? (
+                            <button
+                                className='bg-primaryBlue w-full xsm:max-w-[350px] py-3 px-[34px] text-[#f6f6f6]'
+                                onClick={goToPrevPage}
+                            >
+                                Previous
+                            </button>
+                        ) : (
+                            <div className='w-full max-w-[350px]'></div>
+                        )}
                         <p>{pageNumber}/2</p>
-
-                        {
-                            pageNumber < 2 ?
-                                <button className='bg-primaryBlue w-full max-w-[350px] py-3 px-[34px] text-[#f6f6f6]' onClick={goToNextPage}>
-                                    Next
-                                </button>
-                                :
-                                <div className='w-full max-w-[350px]'></div>
-                        }
+                        {pageNumber < 2 ? (
+                            <button
+                                className='bg-primaryBlue w-full xsm:max-w-[350px] py-3 px-[34px] text-[#f6f6f6]'
+                                onClick={goToNextPage}
+                            >
+                                Next
+                            </button>
+                        ) : (
+                            <div className='w-full max-w-[350px]'></div>
+                        )}
                     </div>
                 </section>
             </div>
@@ -80,8 +77,8 @@ function BlogContent() {
 
 export default function Blog() {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
+        <React.Suspense fallback={<div>Loading...</div>}>
             <BlogContent />
-        </Suspense>
+        </React.Suspense>
     );
 }
--- app/data/blog.ts
+++ app/data/blog.ts
@@ -16,27 +16,27 @@ export const BLOGS: BlogList = [
     },
     {
         date: "December 29, 2023",
-        title: "The evolution of logistics in a connected world",
-        image: '/logistics.jpg'
+        title: "Innovations in smart city mobility",
+        image: '/smart.jpg'
     },
     {
         date: "December 29, 2023",
-        title: "Paving the Way for Eco-Friendly Supply Chains",
-        image: '/supplychain.jpg'
+        title: "Addressing challenges in last-mile delivery",
+        image: '/mile.jpg'
     },
     {
         date: "January 3, 2024",
-        title: "Innovations shaping the future of delivery",
-        image: '/future.jpg'
+        title: "The art of supply chain management in global trade",
+        image: '/global.jpg'
     },
     {
         date: "December 29, 2023",
-        title: "The evolution of logistics in a connected world",
-        image: '/logistics.jpg'
+        title: "Greening the future of freight",
+        image: '/green.jpg'
     },
     {
         date: "December 29, 2023",
-        title: "Paving the Way for Eco-Friendly Supply Chains",
-        image: '/supplychain.jpg'
+        title: "Reducing waste, maximizing resources",
+        image: '/waste.jpg'
     },
 ]
